0ff322b943e3b1de59a476cc343c68bec02380cf
kimwhitestar <102268288+kimwhitestar@users.noreply.github.com>
2022-05-13 14:20:20 +0900

    Add files via upload

--- java/admin/AdminContentCommand.java
+++ java/admin/AdminContentCommand.java
@@ -1,17 +1,33 @@
 package admin;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import board.database.BoardDAO;
+import board.database.BoardVO;
+import guest.database.GuestDAO;
+import guest.database.GuestVO;
+import member.database.MemberDAO;
+import member.database.MemberVO;
+
 public class AdminContentCommand implements AdminInterface {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-
+	  MemberDAO memberDao = new MemberDAO();
+	  List<MemberVO> memberVos = memberDao.searchMemberList(0, 0, 10);
+	  request.setAttribute("memberVos", memberVos);
+	  
+	  BoardDAO boardDao = new BoardDAO();
+	  List<BoardVO> boardVos = boardDao.searchBoardList(0, 10);
+	  request.setAttribute("boardVos", boardVos);
+	  
+	  GuestDAO guestDao = new GuestDAO();
+	  List<GuestVO> guestVos = guestDao.searchGuestList(0, 10);
+	  request.setAttribute("guestVos", guestVos);
 	}
-
 }
--- java/admin/AdminController.java
+++ java/admin/AdminController.java
@@ -45,34 +45,39 @@ public class AdminController extends HttpServlet {
 		}
 		//관리자 Content
 		if (com.equals("adminContent")) {
+			command = new AdminContentCommand();
+			command.execute(request, response);
 			viewPage += "/admin/adminContent.jsp";
 		}
 		//전체회원 게시판
 		else if (com.equals("adminBoardList")) {
 			command = new AdminBoardListCommand();
+			command.execute(request, response);
 			viewPage += "/admin/board/adminBoardList.jsp";
 		}
 		//관리자 대시보드-전체회원 방명록
 		else if (com.equals("adminGuestList")) {
 			command = new AdminGuestListCommand();
+			command.execute(request, response);
 			viewPage += "/admin/guest/adminGuestList.jsp";
 		}
 		//관리자 대시보드-전체회원 목록(공개회원)
 		else if (com.equals("adminMemberList")) {
 			command = new AdminMemberListCommand();
+			command.execute(request, response);
 			viewPage += "/admin/member/adminMemberList.jsp";
 		}
 		//관리자 -전체회원 목록(공개회원)->선택된 회원의 등급 수정
 		else if (com.equals("adminMemberLevelUpdate")) {
 			command = new AdminMemberLevelUpdateCommand();
 			command.execute(request, response);
-			viewPage += "/message/message.jsp";
+			viewPage = "/message/message.jsp";
 		}
 		//관리자 -전체회원 목록(공개회원 중 회원탈퇴신청 후 30일 경과된 회원들)->선택된 회원 삭제
 		else if (com.equals("adminMemberDelete")) {
 			command = new AdminMemberDeleteCommand();
 			command.execute(request, response);
-			viewPage += "/message/message.jsp";
+			viewPage = "/message/message.jsp";
 		}
 		
 		
